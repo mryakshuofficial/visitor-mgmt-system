ed110f11e040d446b4b3d0d42846b2f3a693c2f8
mryakshuofficial <mr.yakshu.official@gmail.com>
2025-07-11 12:39:57 +0530

    Show all visitors list

--- visitors/views.py
+++ visitors/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import tbl_Visitors
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        visiting_whom = request.POST.get("visiting_whom")
+        purpose = request.POST.get("purpose")
+        in_time = request.POST.get("in_time")
+        out_time = request.POST.get("out_time")
+
+        visitor = tbl_Visitors(
+            name=name,
+            visiting_whom=visiting_whom,
+            purpose = purpose,
+            in_time = in_time,
+            out_time = out_time
+        )
+        visitor.save()
+        return redirect('/')
+    visitors = tbl_Visitors.objects.order_by('-id')
+    return render(request,'visitors/index.html',{'visitors':visitors})
