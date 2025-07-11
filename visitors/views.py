1d6bf5bd05b4685986d4434316a6a572209ac900
mryakshuofficial <mr.yakshu.official@gmail.com>
2025-07-11 14:28:32 +0530

    Initial commit: visitor management system with filters WITH month year and whome and add a search baar

--- visitors/views.py
+++ visitors/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render , redirect , get_object_or_404
 from .models import tbl_Visitors
+from django.db.models import Q  # Needed for OR search
+import calendar
+from django.db.models.functions import ExtractMonth, ExtractYear
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':
+        visitors = tbl_Visitors.objects.all()
         name = request.POST.get("name")
         visiting_whom = request.POST.get("visiting_whom")
         purpose = request.POST.get("purpose")
@@ -19,8 +23,43 @@ def index(request):
         )
         visitor.save()
         return redirect('/')
-    visitors = tbl_Visitors.objects.order_by('-id')
-    return render(request,'visitors/index.html',{'visitors':visitors})
+    # SEARCH BAAR START
+    search = request.GET.get('search')
+    if search:
+        visitors = tbl_Visitors.objects.filter(
+            Q(name__icontains=search) | Q(visiting_whom__icontains=search)
+        )
+    else:
+        visitors = tbl_Visitors.objects.all()
+    # SEARCH BAAR END
+
+    # DROPDOWNLIST START
+
+    selected_month = request.GET.get('month')
+    selected_year = request.GET.get('year')
+    selected_whom = request.GET.get('whom')
+
+    if selected_month:
+        visitors = visitors.filter(in_time__month=selected_month)
+    if selected_year:
+        visitors = visitors.filter(in_time__year=selected_year)
+    if selected_whom:
+        visitors = visitors.filter(visiting_whom=selected_whom)
+
+    visitors = visitors.order_by('-id')
+
+    months = [f"{i:02}" for i in range(1, 13)]
+    years = tbl_Visitors.objects.annotate(y=ExtractYear('in_time')).values_list('y', flat=True).distinct()
+    whoms = tbl_Visitors.objects.values_list('visiting_whom', flat=True).distinct()
+
+
+    # visitors = tbl_Visitors.objects.order_by('-id')
+    return render(request, 'visitors/index.html', {
+        'visitors': visitors,
+        'months': months,
+        'years': years,
+        'whoms': whoms,
+    })
 
 def edit(request,id):
     visitor = get_object_or_404(tbl_Visitors, id=id)  # ✅ Don't overwrite 'request'
